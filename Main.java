6471330268ca16667e89f228a7731e6a211016b9
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-09-16 21:46:07 +0000

    finished

--- Main.java
+++ Main.java
@@ -5,28 +5,44 @@ public class Main
     public static void main(String[] args)
     {
         // You can optionally use the Scanner for user input
-        Scanner scan = new Scanner(System.in);
         
-        int numOfNums;
-        System.out.println("How many random numbers do you want to see (must be at least 1)?");
-        numOfNums = scan.nextInt();
-        printRandom(numOfNums);
+        
+
+        
+        printRandom3(15);
+
+
+        System.out.println (calcSlope(3.9, 4.8, 5.1, 1.2));
+
+        System.out.println (roundedDist(-4.5, 12.56));
+
+
     }
     
     public static void printRandom3(int num)
     {
-        // Do problem 1 here
+        int lowerBound = 2;
+        int upperBound = num + 2;
+        System.out.println ( (int) (Math.random() * (upperBound - lowerBound + 1) + lowerBound) );
+        System.out.println ( (int) (Math.random() * (upperBound - lowerBound + 1) + lowerBound) );
+        System.out.println ( (int) (Math.random() * (upperBound - lowerBound + 1) + lowerBound) );
+
     }
     
-    public static double calcSlope(double x1, double x2, double y1, double y2)
+    public static double calcSlope(double x1, double y1, double x2, double y2)
     {
-        // Do problem 2 here; replace code below
-        return 0.0;
+        
+        double slope = (  (y2 - y1) / (x2 - x1) );
+        return (slope);
+
+
     }
     
     public static int roundedDist(double a, double b)
     {
-        // Do problem 3 here; replace code below
-        return 0;
+       int dist = (int)(Math.abs(a - b)); 
+       return (dist);
+
+
     }
 }
